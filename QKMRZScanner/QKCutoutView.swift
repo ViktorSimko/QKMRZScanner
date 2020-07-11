eb4da2fd740721a3f6a9b6fa6f8accfa06678992
Viktor Szilárd Simkó <aqviktor@gmail.com>
2020-07-11 11:58:59 +0200

    add mrz lines to scan view

--- QKMRZScanner/QKCutoutView.swift
+++ QKMRZScanner/QKCutoutView.swift
@@ -7,12 +7,47 @@
 
 import UIKit
 
+enum QKCutoutViewType {
+    case ID
+    case Passport
+}
+
 class QKCutoutView: UIView {
     fileprivate(set) var cutoutRect: CGRect!
+    fileprivate(set) var mrzRect: CGRect!
+    var type: QKCutoutViewType = .ID
+
+    var mrzHeightRatio: CGFloat {
+        if type == .ID {
+            return 3
+        } else {
+            return 5
+        }
+    }
+
+    var mrzNumberOfLines: CGFloat {
+        if type == .ID {
+            return 3
+        } else {
+            return 2
+        }
+    }
+
+    var mrzLine: String {
+        if type == .ID {
+            return "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+        } else {
+            return "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+        }
+    }
+
+    var mrzInset: CGFloat {
+        return cutoutRect.width / mrzHeightRatio / 7
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.black.withAlphaComponent(0.45)
+        //backgroundColor = UIColor.black.withAlphaComponent(0.45)
         contentMode = .redraw // Redraws everytime the bounds (orientation) changes
     }
     
@@ -22,6 +57,7 @@ class QKCutoutView: UIView {
     
     override func draw(_ rect: CGRect) {
         cutoutRect = calculateCutoutRect() // Orientation or the view's size could change
+        mrzRect = calculateMRZRect()
         layer.sublayers?.removeAll()
         drawRectangleCutout()
     }
@@ -37,8 +73,12 @@ class QKCutoutView: UIView {
         
         maskLayer.path = path
         maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
-        
-        layer.mask = maskLayer
+
+        let outsideArea = CALayer()
+        outsideArea.frame = frame
+        outsideArea.backgroundColor = UIColor.black.withAlphaComponent(0.45).cgColor
+        outsideArea.mask = maskLayer
+        layer.addSublayer(outsideArea)
         
         // Add border around the cutout
         let borderLayer = CAShapeLayer()
@@ -46,21 +86,96 @@ class QKCutoutView: UIView {
         borderLayer.path = UIBezierPath(roundedRect: cutoutRect, cornerRadius: cornerRadius).cgPath
         borderLayer.lineWidth = 3
         borderLayer.strokeColor = UIColor.white.cgColor
+        borderLayer.fillColor = UIColor.clear.cgColor
         borderLayer.frame = bounds
         
         layer.addSublayer(borderLayer)
+
+        let rmzLineLayer = CAShapeLayer()
+
+        rmzLineLayer.path = UIBezierPath(roundedRect:
+            CGRect(
+                x: cutoutRect.minX + cutoutRect.width / mrzHeightRatio,
+                y: cutoutRect.minY,
+                width: 1,
+                height: cutoutRect.height
+            ),
+            cornerRadius: 0
+        ).cgPath
+        rmzLineLayer.lineWidth = 1.5
+        rmzLineLayer.strokeColor = UIColor.white.cgColor
+        rmzLineLayer.frame = bounds
+
+        layer.addSublayer(rmzLineLayer)
+
+        for i in 0..<Int(mrzNumberOfLines) {
+            layer.addSublayer(createTextLayer(downOffset: CGFloat(i) * mrzRect.width / mrzNumberOfLines))
+        }
+    }
+
+    fileprivate func createTextLayer(downOffset: CGFloat = 0.0) -> CATextLayer {
+        let text = CATextLayer()
+        text.string = mrzLine
+
+        text.frame = CGRect(
+            x: cutoutRect.minX - cutoutRect.height,
+            y: cutoutRect.maxY - cutoutRect.width / mrzHeightRatio,
+            width: cutoutRect.height,
+            height: cutoutRect.width / mrzHeightRatio
+        ).inset(by: .init(top: mrzInset, left: mrzInset, bottom: mrzInset, right: mrzInset))
+        let rotationPoint = CGPoint(
+            x: cutoutRect.minX,
+            y: cutoutRect.maxY
+        )
+        let c = CGPoint(
+            x: text.frame.midX,
+            y: text.frame.midY
+        )
+
+        var transform = CATransform3DIdentity
+        transform = CATransform3DTranslate(transform, rotationPoint.x - c.x, rotationPoint.y - c.y, 0.0)
+        transform = CATransform3DRotate(transform, CGFloat.pi / 2, 0.0, 0.0, 1.0)
+        transform = CATransform3DTranslate(transform, c.x - rotationPoint.x, c.y - rotationPoint.y, 0.0)
+        transform = CATransform3DTranslate(transform, 0, downOffset, 0.0)
+        text.transform = transform
+        text.contentsScale = UIScreen.main.scale
+
+        var fontSize: CGFloat = 20
+        var font = UIFont(name: "OCRB", size: fontSize)!
+
+        while (mrzLine as NSString).size(withAttributes: [.font: font]).width > text.frame.height {
+            fontSize -= 1
+            font = UIFont(name: "OCRB", size: fontSize)!
+        }
+
+        text.foregroundColor = UIColor.white.withAlphaComponent(0.65).cgColor
+        text.font = font
+        text.fontSize = fontSize
+        text.isWrapped = true
+        text.alignmentMode = .center
+
+        return text
     }
     
     fileprivate func calculateCutoutRect() -> CGRect {
         let documentFrameRatio = CGFloat(1.42) // Passport's size (ISO/IEC 7810 ID-3) is 125mm × 88mm
         let (width, height): (CGFloat, CGFloat)
 
-        height = (bounds.height * 0.7)
-        width = (height / documentFrameRatio)
+        width = (bounds.width * 0.9)
+        height = (width * documentFrameRatio)
         
         let topOffset = (bounds.height - height) / 2
         let leftOffset = (bounds.width - width) / 2
         
         return CGRect(x: leftOffset, y: topOffset, width: width, height: height)
+    }
+
+    fileprivate func calculateMRZRect() -> CGRect {
+        return CGRect(
+            x: cutoutRect.minX,
+            y: cutoutRect.minY,
+            width: cutoutRect.width / mrzHeightRatio,
+            height: cutoutRect.height
+        ).inset(by: .init(top: mrzInset, left: mrzInset, bottom: mrzInset, right: mrzInset))
     }
 }
